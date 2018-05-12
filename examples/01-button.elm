7d5cab3f681ab38cffaf2a10d9d352baf39738b1
Erik Feeley <efeeley@vectorform.com>
2018-05-12 00:08:28 -0400

    elm is the greatest!

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -1,26 +1,31 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
-
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Int
+type alias Model =
+    { counter : Int
+    , message : String
+    }
 
 
 model : Model
 model =
-  0
+    Model 0 ""
 
 
 
@@ -28,18 +33,22 @@ model =
 
 
 type Msg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+    | Reset
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            { model | counter = model.counter + 1 }
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            { model | counter = model.counter - 1 }
+
+        Reset ->
+            { model | counter = 0 }
 
 
 
@@ -48,8 +57,9 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model.counter) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Reset ] [ text "reset" ]
+        ]
--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,14 +1,18 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String
 
 
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
@@ -16,15 +20,15 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
 
 
 model : Model
 model =
-  Model "" "" ""
+    Model "" "" ""
 
 
 
@@ -39,15 +43,15 @@ type Msg
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password }
 
 
 
@@ -56,21 +60,40 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type_ "text", placeholder "Name", onInput Name ] []
-    , input [ type_ "password", placeholder "Password", onInput Password ] []
-    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
+    div []
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "password", placeholder "Password", onInput Password ] []
+        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , viewValidation model
+        ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+    let
+        errors =
+            List.map
+                (\error -> li [ style [ ( "color", "red" ) ] ] [ text error ])
+                (validatePassword model.password model.passwordAgain)
+    in
+    ul [] errors
+
+
+type alias Validation =
+    { condition : Bool
+    , errorMessage : String
+    }
+
+
+validatePassword : String -> String -> List String
+validatePassword pass passAgain =
+    let
+        validations =
+            [ Validation (pass /= passAgain) "Passwords must match"
+            , Validation (String.length pass == 0) "You must enter a password"
+            , Validation (String.length passAgain == 0) "You must confirm your password"
+            ]
+    in
+    validations
+        |> List.filter .condition
+        |> List.map .errorMessage
